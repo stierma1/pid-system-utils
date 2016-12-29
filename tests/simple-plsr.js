7e653ca0b046fe4fed4f0d3ced0e4ab6b7c15518
Mark Stier <stier020@Marks-MacBook-Pro-2.local>
2016-12-28 20:45:49 -0600

    allow for multiple routers

--- tests/simple-plsr.js
+++ tests/simple-plsr.js
@@ -3,39 +3,65 @@ var plsrFn = require("../lib/index").PLSR
 var System = require("pid-system");
 var Tokens = require("../lib/plsr/router/tokens");
 var plsr = {
-  promote: {
-    modules:[
-      {module:"./simple", function:"start", register: "start"},
-      {module:"./simple", function:"one", register:"one"},
-      {module:"./simple", function:"plusOne", register:"plusOne"},
-      {module:"./simple", function:"log", register:"log"}
-    ]
-  },
-  routes:{
-    simple: [{name:"start", eventName:"data", preapply:function(data){
-      return data;
-    }},{name:"one"},
-      {fork:true},
-      {
-        name:"one", 
-        condition: function(d){
-          return Tokens.DROP()
+    promote: {
+        modules: [{
+            function: require("./simple").start,
+            register: "start"
+        }, {
+            function: require("./simple").one,
+            register: "one"
+        }, {
+            function: require("./simple").plusOne,
+            register: "plusOne"
+        }, {
+            function: require("./simple").log,
+            register: "log"
+        }]
+    },
+    router: {
+        name: "router-master2",
+        routes: {
+            simple: [{
+                name: "start",
+                eventName: "data",
+                preapply: function(data) {
+                    return data;
+                }
+            }, {
+                name: "one"
+            }, {
+                fork: true
+            }, {
+                name: "one",
+                condition: function(d) {
+                    return Tokens.CONTINUE()
+                }
+            }, {
+                name: "plusOne"
+            }, {
+                name: "log"
+            }],
+            simple2: [{
+                name: "start",
+                eventName: "data",
+                preapply: function(data) {
+                    return data;
+                }
+            }, {
+                name: "one",
+                condition: function(data) {
+                    return Tokens.REDIRECT("simple");
+                }
+            }]
         }
-      },
-    {name:"plusOne"}, {name:"log"}],
-    simple2: [{name:"start", eventName:"data", preapply:function(data){
-      return data;
-    }}, {name:"one", condition:function(data){
-      return Tokens.REDIRECT("simple");
-    }}]
-  }
+    }
 }
 
 plsrFn(plsr);
 
-setTimeout(function(){
-  System.resolve("start")
-  .then(function(pid){
-    pid.emit("data", [])
-  })
+setTimeout(function() {
+    System.resolve("start")
+        .then(function(pid) {
+            pid.emit("data", [])
+        })
 }, 500)
